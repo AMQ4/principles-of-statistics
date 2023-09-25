f5bc0c635fa4326630e6f5f4ca96044c67ca9df2
Ahmad <amq4319@gmail.com>
2023-09-25 16:38:34 +0300

    implement some methods

--- DescriptiveStatistics/freqtable/freq.py
+++ DescriptiveStatistics/freqtable/freq.py
@@ -1,11 +1,13 @@
 import collections
+import heapq
 from enum import Enum
 from abc import ABC, abstractmethod
+from typing import Callable, Any
 
 
 class SortType(Enum):
-    BY_FREQ = 0
-    BY_ITEM = 1
+    BY_ITEM = 0
+    BY_FREQ = 1
 
 
 def unique_copy(input_list):
@@ -37,30 +39,43 @@ def unique_copy(input_list):
 
 
 class FrequencyTable(ABC):
-    def __init__(self, data):
-        self._data = data
-        self._table = self._calculate_frequencies()
-        self._frequencies = list(self._table.values())
+    def __init__(self, data=None):
+        self._table = self._calculate_frequencies(data if data is not None else [])
 
     @abstractmethod
-    def _calculate_frequencies(self):
+    def _calculate_frequencies(self, data):
         pass
 
     @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def mean(self):
+        pass
+
+    @abstractmethod
+    def median(self):
+        pass
+
+    @abstractmethod
+    def mode(self):
+        pass
+
     def display_table(self):
-        pass
+        print(self.__str__())
 
     def to_dict(self):
         return self._table.copy()
 
     def get_data(self):
-        return self._data.copy()
+        return self._table.keys()
 
     def get_frequencies(self):
-        return self._frequencies.copy()
+        return self._table.values()
 
     def get_total(self):
-        return sum(self._frequencies)
+        return sum(self._table.values())
 
     def sort(self, by=SortType.BY_FREQ, acs=True):
         try:
@@ -71,19 +86,100 @@ class FrequencyTable(ABC):
         else:
             self._table = dict(sorted(self._table.items(), key=lambda item: item[by.value], reverse=not acs))
 
+    def append(self, data_to_append):
+        counter = collections.Counter(data_to_append)
+        for item in counter:
+            if item in self._table:
+                self._table[item] += counter[item]
+            else:
+                self._table[item] = counter[item]
+
+    def merge(self, freqtable: 'FrequencyTable'):
+        for item, freq in freqtable._table.items():
+            self.append([item] * freq)
+
+    def filter_by_freq(self, min_frequency, max_frequency):
+        freqrange = range(min_frequency, max_frequency + 1)
+        freqtable = self.__class__()
+
+        for item, freq in self._table.items():
+            if freq in freqrange:
+                freqtable.append([item] * freq)
+
+        return freqtable
+
+    def filter_by_class(self, predicate: Callable[[Any, Any], bool]):
+        freqtable = self.__class__()
+        for item, freq in self._table.items():
+            if predicate(item, freq):
+                freqtable.append([item] * freq)
+        return freqtable
+
     def __len__(self):
-        return len(self._data)
+        return len(self._table.keys())
+
+    def get_subset(self, subset_elements):
+        freqtable = self.__class__()
+        for element in subset_elements:
+            freq = self._table.get(element)
+            freqtable.append([element] * (freq if freq is not None else 0))
+        return freqtable
+
+    def get_top_n_elements(self, n):
+        freqtable = self.__class__()
+        top_n_elements = heapq.nlargest(n, self._table.items(), lambda item: item[1])
+
+        for item, freq in top_n_elements:
+            freqtable.append([item] * freq)
+
+        return freqtable
+
+    def get_lowest_n_elements(self, n):
+        freqtable = self.__class__()
+        top_n_elements = heapq.nsmallest(n, self._table.items(), lambda item: item[1])
+
+        for item, freq in top_n_elements:
+            freqtable.append([item] * freq)
+
+        return freqtable
+
+    def map_elements(self, func: Callable[[Any], Any]):
+        items = list(self._table.keys())
+        for item in items:
+            new_item = func(item)
+            freq = self._table[item]
+            self._table.pop(item)
+            if self._table.get(new_item) is not None:
+                self._table[new_item] += freq
+            else:
+                self._table[new_item] = freq
+
+    def apply_frequency_operation(self, func: Callable[[Any], Any]):
+        items_with_neg_freq = []
+        for item, freq in self._table.items():
+            newfreq = func(freq)
+            try:
+                if newfreq <= 0:
+                    raise ValueError(f"{item} deleted since new frequency, {newfreq} <= 0")
+            except ValueError as e:
+                print(e)
+                items_with_neg_freq.append(item)
+            else:
+                self._table[item] = newfreq
+
+        for item in items_with_neg_freq:
+            self._table.pop(item)
 
 
 class DiscreteFrequencyTable(FrequencyTable):
-    def __init__(self, data):
+    def __init__(self, data=None):
         super().__init__(data)
 
-    def _calculate_frequencies(self):
-        counter = collections.Counter(self._data)
+    def _calculate_frequencies(self, data):
+        counter = collections.Counter(data)
         return dict(counter.items())
 
-    def display_table(self):
+    def __str__(self):
         max_len = 0
 
         for class_item in self._table:
@@ -91,9 +187,33 @@ class DiscreteFrequencyTable(FrequencyTable):
 
         max_len += 10
 
-        print(f"{'Class':<{max_len}}", "Frequency")
+        table = ""
+        table = table + f"{'Class':<{max_len}}" + "Frequency"
+
         for class_item in self._table:
-            print(f"{class_item:<{max_len}}", self._table[class_item])
+            table = table + f"\n{class_item:<{max_len}}" + str(self._table[class_item])
+
+        return table
+
+    def mean(self):
+        sum = 0
+        for item, freq in self._table.items():
+            sum += item * freq
+
+        return sum / self.get_total()
+
+    def median(self):
+        items = []
+        for item, freq in self._table.items():
+            items = items + [item] * freq
+
+        items.sort()
+
+        return items[int(self.get_total() / 2)] if self.get_total() % 2 == 1 else (
+                (items[int(self.get_total() / 2)] + items[int(self.get_total() / 2) - 1]) / 2)
+
+    def mode(self):
+        return list(self.get_top_n_elements(1).get_data())[0]
 
 
 class EqualClassLengthFrequencyTable(FrequencyTable):
